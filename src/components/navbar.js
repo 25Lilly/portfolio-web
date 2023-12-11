c0ac52977471ae26ce626b04453cdaae5868ae2e
Lilly Chiavetta <lilly.chiavetta21@gmail.com>
2023-12-10 21:10:23 -0500

    Added SeeFirst section

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import { click } from '@testing-library/user-event/dist/click'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import { Button } from './Button'
@@ -19,13 +19,15 @@ function Navbar() {
     }
   }
 
+  useEffect(() => { showButton() }, [])
+
   window.addEventListener('resize', showButton)
 
   return (
     <>
     <nav className="navbar">
         <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                 LHC <img src="sandwich.png" alt="sand" width="22" height="17"></img>
             </Link>
             <div className="menu-icon" onClick={handleClick}>
@@ -48,7 +50,7 @@ function Navbar() {
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="/Games" className="nav-links" onClick={closeMobileMenu}>
+                    <Link to="/Games" className="nav-links-mobile" onClick={closeMobileMenu}>
                         Games! 
                     </Link>
                 </li>
