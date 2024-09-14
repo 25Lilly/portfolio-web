52b13ceac24e62a2559f62b1d019a4db1e8bc92c
Lilly Chiavetta <lhc24@duke.edu>
2024-09-13 22:30:07 -0400

    added word descriptors changes

--- src/components/SeeFirst.js
+++ src/components/SeeFirst.js
@@ -4,9 +4,13 @@ import { Button } from './Button';
 import './SeeFirst.css';
 import images from '../coverImageLoader';
 
+const words = ['Innovator', 'Explorer', 'Problem Solver', 'Artist', 'Developer', 'Programmer', 'Maker', 'Thinker', 'Collaborator', 'Leader', 'Learner', 'Creator', 'Builder'];
+let word = words[0];
+
 function SeeFirst() {
   //continuously add pop up images to the first container
   let imageIndex = 0;
+  let wordIndex = 0;
   setInterval(() => {
     //if the first container is not visible, don't change the background
     if (document.querySelector('.first-container') === null) {
@@ -14,6 +18,11 @@ function SeeFirst() {
     }
     let image = images[imageIndex];
     document.querySelector('.first-container').style.background = `linear-gradient(to right, rgba(0, 0, 0, 0.1) 50%, rgba(0, 0, 0, 1) 75%), url(${image}) center center/cover no-repeat`;
+    
+    let word = words[wordIndex];
+    document.querySelector('.first-container p').innerHTML = `Engineer, Designer, ${word}`;
+    
+    wordIndex = (wordIndex + 1) % words.length;
     imageIndex = (imageIndex + 1) % images.length;
   }, 6000);
 
