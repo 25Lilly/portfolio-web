18d63e02ffc7ae9a863fed55fd8069d503daa23f
Lilly Chiavetta <lhc24@duke.edu>
2024-09-13 22:08:54 -0400

    Added cover image transistions

--- src/components/SeeFirst.js
+++ src/components/SeeFirst.js
@@ -2,13 +2,25 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './SeeFirst.css';
+import images from '../coverImageLoader';
 
 function SeeFirst() {
+  //continuously add pop up images to the first container
+  let imageIndex = 0;
+  setInterval(() => {
+    //if the first container is not visible, don't change the background
+    if (document.querySelector('.first-container') === null) {
+      return;
+    }
+    let image = images[imageIndex];
+    document.querySelector('.first-container').style.background = `linear-gradient(to right, rgba(0, 0, 0, 0.1) 50%, rgba(0, 0, 0, 1) 75%), url(${image}) center center/cover no-repeat`;
+    imageIndex = (imageIndex + 1) % images.length;
+  }, 6000);
+
   return (
     <div className='first-container'>
-      {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
       <h1>Lilly Chiavetta</h1>
-      <p>engineer</p>
+      <p> Engineer, Designer, Creator </p>
       <div className='first-btns'>
         <Button
           className='btns'
@@ -20,5 +32,7 @@ function SeeFirst() {
     </div>
   );
 }
+
+
 
 export default SeeFirst;
---
+++ src/coverImageLoader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    images = Object.values(images);
+    return images;
+  }
+  
+const images = importAll(require.context('/public/images/cover_images', false, /\.(jpg|jpeg|png)$/));
+export default images;
+  
